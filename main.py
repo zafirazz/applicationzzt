0818f77e9e06be8c8720e31b6cda0ba938d87774
Zafira <zafiraibraeva@gmail.com>
2022-05-22 17:58:44 +0600

    added buttons new and exit

--- main.py
+++ main.py
@@ -1,180 +1,121 @@
+import tkinter as tk
+from PIL import Image, ImageTk
 import os
-from tkinter import *
-from PIL import ImageTk, Image
 
-class App:
-    def __init__(self, root, papka):
-        self.papka = papka
-        self.root = root
-        self.root.geometry("700x600")
-        p = os.getcwd()
-        os.chdir(p + self.papka)
-        p = os.getcwd()
-        self.spisok = os.listdir(p)
-        self.l = 0
-        self.r = 1
-        #lbls
-        self.main_lbl = Label(self.root, text="Welcome to game THIS OR THAT!")
-        self.im = Image.open(self.spisok[self.l])
-        self.im = ImageTk.PhotoImage(self.im)
-        self.im_lbl = Label(self.root, image=self.im)
-        self.im_lbl.pack(side='left')
-        self.imr = Image.open(self.spisok[self.r])
-        self.imr = ImageTk.PhotoImage(self.imr)
-        self.imr_lbl = Label(self.root, image=self.imr)
-        self.imr_lbl.pack(side='right')
-        #btns
-        self.b1 = Button(self.root, text="left", command=self.left)
-        self.b1.place(x=0, y=454)
-        self.b2 = Button(self.root, text="right", command=self.right)
-        self.b2.place(x=635, y=454)
-        self.btn_new = Button(self.root, text='New', command=self.menu)
-        self.btn_exit = Button(self.root, text='Exit', command=self.root.destroy)
-        #txts
-        self.win = Toplevel()
-        self.txt = Text(self.root, height=20, width=80)
-        self.txt_lbl = Label(self.root, text='Here is your #1 Book!')
-        self.btn_books = Button(self.win, text='Еop books', command=self.top_book)
-        self.win_label = Label(self.win, text="pick one", bg='black')
+LARGEFONT = ("Verdana", 35)
 
-    def menu(self):
-        self.win_label.pack(side='center')
-        self.menubtn.pack(side='bottom')
+class App(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
 
-    def top_book(self):
-        self.papka = '/topbooks'
-        self.l = 0
-        self.r = 1
-        self.left()
-        self.right()
+    def open_frame(self):
+        self.lift()
 
-    def top_movie(self):
-        pass
+class TopBooks(App):
+   def __init__(self, *args, **kwargs):
+       App.__init__(self, *args, **kwargs)
 
-    def left(self):
-        if (self.r == (len(self.spisok) - 1) or self.l == (len(self.spisok) - 1)):
-            self.imr_lbl.after(1000, self.imr_lbl.destroy())
-            self.b2.destroy()
-            self.b1.destroy()
-            self.im_lbl.pack(side='top')
-            self.txt_lbl.pack(side='bottom')
-            self.btn_new.pack(side='left')
-            self.btn_exit.pack(side='right')
+       self.papka = "/topbooks"
+       p = os.getcwd()
+       os.chdir(p + self.papka)
+       p = os.getcwd()
+       self.spisok = os.listdir(p)
+       self.l = 0
+       self.r = 1
 
-        if self.r < self.l:
-            self.r = self.l + 1
-            self.imr = Image.open(self.spisok[self.r])
-            self.imr = ImageTk.PhotoImage(self.imr)
-            self.imr_lbl.config(image=self.imr)
-        else:
-            self.r += 1
-            self.imr = Image.open(self.spisok[self.r])
-            self.imr = ImageTk.PhotoImage(self.imr)
-            self.imr_lbl.config(image=self.imr)
+       #img left
+       self.im = Image.open(self.spisok[self.l])
+       self.im = ImageTk.PhotoImage(self.im)
+       self.im_lbl = tk.Label(self, image=self.im, bg="#D4BAEC")
+       self.im_lbl.image = self.im
+       self.im_lbl.pack(side="left")
 
-    def right(self):
-        if (self.r == (len(self.spisok) - 1) or self.l == (len(self.spisok) - 1)):
-            self.im_lbl.after(1000, self.im_lbl.destroy())
-            self.b1.destroy()
-            self.b2.destroy()
-            self.imr_lbl.pack(side='top')
-            self.txt_lbl.pack(side='bottom')
-            self.btn_new.pack(side='left')
-            self.btn_exit.pack(side='right')
+       #img right
+       self.img = Image.open(self.spisok[self.r])
+       self.img = ImageTk.PhotoImage(self.img)
+       self.img_lbl = tk.Label(self, image=self.img, bg="#D4BAEC")
+       self.img_lbl.image=self.img
+       self.img_lbl.pack(side="right")
 
-        if self.l < self.r:
-            self.l = self.r + 1
-            self.im = Image.open(self.spisok[self.l])
-            self.im = ImageTk.PhotoImage(self.im)
-            self.im_lbl.config(image=self.im)
-        else:
-            self.l += 1
-            self.im = Image.open(self.spisok[self.l])
-            self.im = ImageTk.PhotoImage(self.im)
-            self.im_lbl.config(image=self.im)
+       #buttons
+
+       btn_left = tk.Button(self, text="Left", command=lambda: self.left())
+       btn_left.pack(side="left")
+
+       btn_right = tk.Button(self, text="Right", command=lambda: self.right())
+       btn_right.pack(side="right")
+
+       label = tk.Label(self, text="Pick only one!", font=LARGEFONT)
+       label.pack(side="top", fill="both", expand=True)
+
+   def left(self):
+       if (self.r == (len(self.spisok) - 1) or self.l == (len(self.spisok) - 1)):
+           self.img_lbl.after(1000, self.img_lbl.destroy())
+           self.btn_left.destroy()
+           self.btn_right.destroy()
+           self.bks_lbl.destroy()
+           self.im_lbl.pack(side='top')
+
+       if self.r < self.l:
+           self.r = self.l + 1
+           self.img = Image.open(self.spisok[self.r])
+           self.img = ImageTk.PhotoImage(self.img)
+           self.img_lbl.image=self.img
+           self.img_lbl.config(image=self.img)
+       else:
+           self.r += 1
+           self.img = Image.open(self.spisok[self.r])
+           self.img = ImageTk.PhotoImage(self.img)
+           self.img_lbl.image=self.img
+           self.img_lbl.config(image=self.img)
+
+   def right(self):
+       if (self.r == (len(self.spisok) - 1) or self.l == (len(self.spisok) - 1)):
+           self.im_lbl.after(1000, self.im_lbl.destroy())
+           self.btn_left.destroy()
+           self.btn_right.destroy()
+           self.img_lbl.pack(side='top')
+
+       if self.l < self.r:
+           self.l = self.r + 1
+           self.im = Image.open(self.spisok[self.l])
+           self.im = ImageTk.PhotoImage(self.im)
+           self.im_lbl.image=self.im
+           self.im_lbl.config(image=self.im)
+       else:
+           self.l += 1
+           self.im = Image.open(self.spisok[self.l])
+           self.im = ImageTk.PhotoImage(self.im)
+           self.im_lbl=self.im
+           self.im_lbl.config(image=self.im)
 
 
+class MainView(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        p1 = App(self)
+        p2 = TopBooks(self)
 
-root = Tk()
-root.resizable(width=False, height=False)
-ob = App(root, '/topbooks')
-#game = Open(root)
-##ob = App(root, '/topbooks')
+        btn_frame = tk.Frame(self)
+        container = tk.Frame(self)
+        btn_frame.pack(side="top", fill="x", expand=False)
+        container.pack(side="top", fill="both", expand=True)
+
+        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+
+        b1 = tk.Button(btn_frame, text="menu", command=p1.open_frame)
+        b2 = tk.Button(btn_frame, text="top books", command=p2.open_frame)
+
+        b1.pack(side="top")
+        b2.pack(side="top")
+        p1.open_frame()
+
+root = tk.Tk()
+main = MainView(root)
+main.pack(side="top", fill="both", expand=True)
+root.wm_geometry("700x700")
 root.mainloop()
-
-
-#Pages with frames
-# import tkinter as tk
-# from PIL import ImageTk, Image
-# import os
-#
-# LARGEFONT = ("Verdana", 35)
-#
-# class App(tk.Tk):
-#     def __init__(self, *args, **kwargs):
-#         tk.Tk.__init__(self, *args, **kwargs)
-#         container = tk.Frame(self)
-#         container.pack(side="top", fill="both", expand=True)
-#
-#         container.grid_columnconfigure(0, weight=1)
-#         container.grid_columnconfigure(0, weight=1)
-#
-#         self.frames = {}
-#
-#         for f in (Menu, TopBooks, TopMovie):
-#             frame = f(container, self)
-#             self.frames[f]=frame
-#             frame.grid(row=0, column=0, sticky='n')
-#         self.open_frame(Menu)
-#
-#     def open_frame(self, controller):
-#         frame = self.frames[controller]
-#         frame.tkraise()
-#
-#
-# class Menu(tk.Frame):
-#     def __init__(self, parent, controller):
-#         tk.Frame.__init__(self, parent)
-#         menu_lbl = tk.Label(self, text="Menu Page", font=LARGEFONT)
-#         menu_lbl.pack(pady=40, padx=40)
-#
-#         btn1 = tk.Button(self, text="Top Books", command=lambda : controller.open_frame(TopBooks))
-#         btn1.pack(side="bottom")
-#         btn2 = tk.Button(self, text="Top Movies", command=lambda : controller.open_frame(TopMovie))
-#         btn2.pack(side="bottom")
-#
-# class TopBooks(tk.Frame):
-#     def __init__(self, parent, controller):
-#         tk.Frame.__init__(self, parent)
-#
-#         self.papka = "/top_books"
-#         p = os.getcwd()
-#         os.chdir(p + self.papka)
-#         p = os.getcwd()
-#         self.spisok = os.listdir(p)
-#         l = 0
-#         r = 1
-#
-#         bks_lbl = tk.Label(self, text="Pick only one!", font=LARGEFONT)
-#         bks_lbl.pack(padx=50, pady=50)
-#
-#         im = tk.PhotoImage(file="/home/marbelle/PycharmProjects/intro_project/top_books/I-KISSED-SHARA-WHEELER-by-Casey-McQuiston.jpg")
-#         tk.Label(image=im).pack(side='left')
-#         btn_bks = tk.Button(self, text="Back to menu", command=lambda : controller.open_frame(Menu))
-#         btn_bks.pack(padx=60, pady=60)
-#
-#
-# class TopMovie(tk.Frame):
-#     def __init__(self, parent, controller):
-#         tk.Frame.__init__(self, parent)
-#         mvs_lbl = tk.Label(self, text="Pick only one!", font=LARGEFONT)
-#         mvs_lbl.pack(padx=50, pady=50)
-#
-#         btn_mvs = tk.Button(self, text="Back to menu", command=lambda : controller.open_frame(Menu))
-#         btn_mvs.pack(padx=60, pady=60)
-#
-# app = App()
-# app.mainloop()
 
 
 
